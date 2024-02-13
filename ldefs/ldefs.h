06a92695e5a3c92a80377ff9a9f82fa457882e4f
robotsvision <robotsvision.vx@gmail.com>
2024-02-13 21:58:42 +0600

    lthreads -> basic functions lapp -> lexit function and codes

--- ldefs/ldefs.h
+++ ldefs/ldefs.h
@@ -14,6 +14,23 @@
 #ifdef __LDEFS_SPT__
 #define LMAIN _LMAIN 
 /* _LMAIN is defined in native library */
+
+/* Appilication exit */
+
+/* Standard libraries */
+#include <stdint.h>
+
+/* Type */
+typedef uint8_t lexit_code_t;
+
+enum lexit_codes {
+    LENA_EXIT_SUCCESS = 0x00,
+    LENA_EXIT_FAILURE = 0x01,
+    // ... other codes (max = 0xFF/255)
+};
+
+extern void lexit(lexit_code_t code);
+
 #else
 #error "[llibs.h] -> [ldefs.h]: Application signature is undefined! Llibs doesn't support this platform!"
 #endif
---
+++ lthreads/lthreads.h
@@ -0,0 +1,24 @@
+#ifndef __L_DEFS_H__
+#define __L_DEFS_H__
+
+/**************************************************************************//**
+ * @file     ldefs.h
+ * @brief    Project L - Cross-platform defs library (for Lena lang)
+ * @version  V0.0.1
+ * @date     10. Feb 2024
+ ******************************************************************************/
+
+/* Include platform selector */
+#include "platforms/platform.h"
+
+#ifdef __LTHREADS_SPT__
+
+extern lthread_t lthread_create(lthread_func_ptr_t func);
+extern lthread_error_t lthread_get_error(lthread_t* thread);
+extern void lthread_join(lthread_t* thread, lthread_wait_t wait_ms);
+
+#else
+#error "[llibs.h] -> [lthreads.h]: Current platform doesn't support thread functions!"
+#endif
+
+#endif // __L_DEFS_H__
